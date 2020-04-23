ad47752d5b4d1e983c4ab04f2d16de87fbc9c4e6
Apoorva Choudhary <apoorva@Apoorvas-MacBook-Pro.local>
2020-04-23 20:02:17 +0530

    install sqlalchemy

--- application.py
+++ application.py
@@ -1,5 +1,13 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@book-recommender-db.ckt9u6nlihrw.us-east-1.rds.amazonaws.com:3306/book-recommender-db'
+db = SQLAlchemy(application)
+
+# change this to your own value
+application.secret_key = 'cC1YCIWOj9GgWspgNEo2'
 
 @application.route("/")
 def hello():
@@ -7,4 +15,3 @@ def hello():
 
 if __name__ == '__main__':
     application.run()
-
