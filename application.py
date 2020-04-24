51abeb30e34a5ae3d0063d0cb79a69d7a661b4b8
Apoorva Choudhary <apoorva@Apoorvas-MacBook-Pro.local>
2020-04-24 11:26:23 +0530

    app update

--- application.py
+++ application.py
@@ -1,30 +1,15 @@
 from flask import Flask
-# from config import Config
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__)
 CORS(application)
-# application.config.from_object(Config)
-db = SQLAlchemy(application)
+#db = SQLAlchemy(application)
 
 
 
 @application.route("/")
-def BooksList():
-    return  {
-                    "bookslist": [ {"name":"Wings of Fire",
-                    "author":"A P J Abdul Kalam, Arun Tiwari"
-                    },
-                    {"name":"Harry Potter and the Half-Blood Prince",
-                    "author":"J K Rowling"
-                    },
-                    {"name":"Long Walk to freedom",
-                    "author":"Nelson Mandela"
-                    },
-                    {"name":"Harry Potter and the Half-Blood Prince",
-                    "author":"J K Rowling"
-                    } ]
-                }
+def hello():
+    return "hello world!!"
 
 
 
