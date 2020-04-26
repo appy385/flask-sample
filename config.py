72fd3000623eaea147f6f1aa8d4edb60d0c176d1
Apoorva Choudhary <apoorva@Apoorvas-MacBook-Pro.local>
2020-04-26 15:05:14 +0530

    ACMS database uri

---
+++ config.py
@@ -0,0 +1,7 @@
+
+
+SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://root:admin123@book-recommender-db.clywrivu0ofa.ap-south-1.rds.amazonaws.com:3306/bookRecommender'
+SQLALCHEMY_TRACK_MODIFICATIONS= False
+
+# change this to your own value
+#secret_key = 'cC1YCIWOj9GgWspgNEo2'
