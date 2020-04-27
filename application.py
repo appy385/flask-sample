a6a942dd8132aa60656feae3991245c828f35cea
Apoorva Choudhary <choudhary.apoorva385@gmail.com>
2020-04-27 19:31:49 +0530

    complete genre api

--- application.py
+++ application.py
@@ -31,12 +31,17 @@ def bookList():
             }
 @application.route("/<genre>")
 def genre(genre):
-    # books=db.session.query(Book).limit(10)
-    # for b in books:
-    #     print(b.average_rating)
-    # books= Book.query.order_by(func.rand()).limit(1)
-    # print()
-    return "hello world"
+    books= Book.query.filter_by(genre=genre).order_by(func.rand()).limit(10).all()
+    booksList=[]
+
+    for b in books:
+        book = {"book_id": b.book_id, "goodreads_book_id": b.goodreads_book_id, "authors": b.authors, "isbn": b.isbn, "title": b.title,
+        "average_rating": b.average_rating, "image_url": b.image_url, "genre": b.genre }
+        booksList.append(book)
+
+    booksDict={ "bookslist": booksList}
+
+    return booksDict
 
 
 
